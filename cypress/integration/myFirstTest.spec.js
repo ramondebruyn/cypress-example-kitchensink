d31f15d31dfe28996cb7b7cceb2949350d8e253d
ramondebruyn <78156608+ramondebruyn@users.noreply.github.com>
2022-05-13 23:24:07 -0400

    Update h1 test

--- cypress/integration/myFirstTest.spec.js
+++ cypress/integration/myFirstTest.spec.js
@@ -69,6 +69,25 @@ context('My First Test', () => {
     })
   })
 
+  it('renders the correct h1 text', () => {
+    cy.get('h1').should('contain.text', 'Actions')
+  })
+
+  it('has an h1 on the page', () => {
+    cy.get('h1').should('exist')
+  })
+
+  it('renders a paragraph under the h1 tag', () => {
+    cy.get('.container').eq(1).find('p').should('exist')
+  })
+
+  it('renders a section with the correct elements', () => {
+    cy.get('.container').eq(2).within(() => {
+      cy.get('h4').should('exist')
+      cy.get('p').should('exist')
+    })
+  })
+
   //   before(() => {
   //     cy.request('https://api.spacexdata.com/v3/missions').its('body').should('have.length', 10)
   //   })

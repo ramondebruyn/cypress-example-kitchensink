ea3391897b908a1eb5074346eb955cfd99156434
ramondebruyn <78156608+ramondebruyn@users.noreply.github.com>
2022-05-16 02:12:46 -0400

    Update from .side test versions

--- cypress/integration/myFirstStarlightTest.spec.js
+++ cypress/integration/myFirstStarlightTest.spec.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /// <reference types="cypress-xpath" />
 // const { before } = require("cypress/types/lodash")
 
@@ -57,7 +58,7 @@ context('Contact Us Test', () => {
     // still need to verify
   })
 
-  it('can book a demo', () => {
+  it('can book a demo', async function () {
     // this event will automatically be unbound when this
   // test ends because it's attached to 'cy'
     cy.on('uncaught:exception', (err, runnable) => {
@@ -109,6 +110,8 @@ context('Contact Us Test', () => {
     cy.get('.calendly-container').should('exist')
     cy.wait(30)
     cy.get('.LYc2_mbhXohr_BOepbm2').should('exist')
+
+    // ...continue here to figure out how to test calendly
     // cy.get('.calendly-container').within(() => {
     //   cy.get('iframe').should('exist').within(() => {
     //     cy.get('div').should('exist')
@@ -116,15 +119,172 @@ context('Contact Us Test', () => {
     // })
   })
 
-  it('customer can search for and book a band', () => {
+  it('customer can search for and book a band', async function () {
+    cy.visit('https://staging.starlightmusic.com/')
+    // get event date
 
+    /// get event date, store event date
+    cy.xpath('//div[@id=\"__next\"]/div/main/div/header/div[3]/div[3]/section/div/div/div/div/input').then((element) => {
+      const eventDate = element.val()
+
+      cy.wrap(eventDate).as('eventDate')
+      console.log('Event Date: ', eventDate)
+    })
+
+    // click event date
+    cy.xpath('//div[@id=\"__next\"]/div/main/div/header/div[3]/div[3]/section/div/div/div/div/input').click()
+
+    cy.get('.ant-picker-month-btn').click()
+    cy.get('tr:nth-child(2) > .ant-picker-cell:nth-child(3) > .ant-picker-cell-inner').click()
+    cy.get('.ant-picker-year-btn').click()
+    cy.get('.ant-picker-cell-selected > .ant-picker-cell-inner').click()
+
+    ///
+    cy.xpath('//div[@id=\"__next\"]/div/main/div/header/div[3]/div[3]/section/div/div[2]/span/input').click()
+    cy.find('.ant-tooltip-open > .search_box_field_auto').click()
+    cy.find('.ant-tooltip-open > .search_box_field_auto').dblclick()
+    ///
+
+    cy.get('.ant-tooltip-open > .search_box_field_auto').click()
+    cy.get('.ant-tooltip-open > .search_box_field_auto').type('Plaza Hotel{enter}')
+    cy.get('.ant-tooltip-open > .search_box_field_auto').type('Cypress.io{enter}')
+    // console.log(vars["event_date"])
+    cy.get('.d-flex:nth-child(1) > .Header_search_btn__1DsYn > img').click()
+    ///
+    cy.get('.SearchResult_search_result_heading__20s2l').should('contains.text', 'Congratulations!')
+    cy.xpath('//button[contains(.,\"Explore\")]').should('have.length.gt', 0)
+    ///
+    cy.get('.SearchResult_search_result_card__2m0Pk:nth-child(2) .SearchResult_search_result_card_price__36J67').click()
+    cy.get('.SearchResult_search_result_card__2m0Pk:nth-child(2) img').click()
+    ///
+    cy.get('.band_detail_nav_item:nth-child(3) > .m-0').click()
+    ///
+    cy.get('.BandConfiguration_price_tab__1cyLl').click({ force: true })
+    cy.get('.BandConfiguration_band_conf_pink_bg__1OldT').click({ force: true })
+    /// band price
+    cy.get('.BandConfiguration_tab__Epc4Q:nth-child(2) span:nth-child(1)').then((element) => {
+      const bandPrice = element.text()
+
+      cy.wrap(bandPrice).as('bandPrice')
+
+      console.log('band price: ', bandPrice)
+    })
+
+    cy.get('@eventDate').then((eventDate) => {
+      const eventMonth = eventDate.substring(0, 2)
+      const eventDay = eventDate.substring(3, 5)
+      const eventYear = eventDate.substring(6)
+
+      cy.wrap(eventMonth).as('eventMonth')
+      cy.wrap(eventDay).as('eventDay')
+      cy.wrap(eventYear).as('eventYear')
+
+      console.log('Month: ', eventMonth, ', Day: ', eventDay, ', Year: ', eventYear)
+    })
+
+    cy.visit('https://starbridge.starlightmusic.com/')
   })
 
-  it('event planner can login', () => {
+  it('event planner can login', async function () {
+    cy.visit('https://staging.starlightmusic.com/')
 
+    cy.xpath('//p[@class=\"Header_nav_section_profile_detail_welcomeText__Tl1wI\"]').then((value) => {
+      const welcomeMessageCount = value.length()
+
+      if (welcomeMessageCount === 0) {
+        cy.xpath('//li[contains(.,\"Logout\")]').should('exist').click()
+        cy.xpath('//p[contains(.,\"Welcome\")]').should('exist')
+      }
+    })
+
+    cy.findAllByText('Login /Sign Up').first().should('contain.text', 'Login /Sign Up')
+    cy.contains('Login /Sign Up').click()
+    cy.get('*[data-test=\"user_name\"]').type('srogers.dl0@gmail.com')
+    cy.get('*[data-test=\"password\"]').type('CaptainAmerica501!')
+    cy.get('.Header_login_btn__2tigy').click()
+
+    cy.xpath('//button[contains(.,\"Verify\")]').should('exist')
+
+    /// get otp here START ///
+    cy.visit('https://oksms.org/receive/15186726861')
+    /// get otp here END ///
+
+    cy.xpath('//button[contains(.,\"Verify\")]').should('exist')
+    const userOtp = 1234
+
+    console.log('userOtp: ', userOtp)
+    cy.get('@userOtp').then((value) => {
+      const otpArray = userOtp.split('')
+      const otpDigit1 = otpArray[0]
+      const otpDigit2 = otpArray[1]
+      const otpDigit3 = otpArray[2]
+      const otpDigit4 = otpArray[3]
+
+      cy.wrap(otpDigit1).as('otpDigit1')
+      cy.wrap(otpDigit2).as('otpDigit2')
+      cy.wrap(otpDigit3).as('otpDigit3')
+      cy.wrap(otpDigit4).as('otpDigit4')
+
+      console.log('otpDigits: ', otpDigit1, ' -- ', otpDigit2, ' -- ', otpDigit3, ' -- ', otpDigit4)
+
+      cy.get('.Header_otp_input_form__303Uy:nth-child(1) > input').type(otpDigit1)
+      cy.get('.Header_otp_input_form__303Uy:nth-child(2) > input').type(otpDigit2)
+      cy.get('.Header_otp_input_form__303Uy:nth-child(3) > input').type(otpDigit3)
+      cy.get('.Header_otp_input_form__303Uy:nth-child(4) > input').type(otpDigit4)
+    })
+
+    cy.get('.Header_login_btn__2tigy:nth-child(2)').click()
+    cy.xpath('//p[contains(.,\"Welcome\")]').should('contain.text', 'Welcome')
   })
 
-  it('event planner signup', () => {
+  it('event planner signup', async function () {
+    cy.visit('https://staging.starlightmusic.com/')
+    cy.contains('Login / Signup').click()
+    cy.get('#firstName').click()
+    cy.get('#firstName').click()
+    cy.get('#firstName').type('Bruce')
+    cy.get('#lastName').type('Bruce')
+    cy.get('.Header_input_section__1HbL7:nth-child(3) #user_email').type('bbanner.dl@gmail.com')
+    cy.get('input[name=phone]').type('(518) 672-6861')
+    cy.get('*[data-test=\"user_password\"]').type('TheHulk501!')
+    cy.get('.Header_register_checkbox__2Grz-').click()
+    cy.get('#companyName').type('Test Event Planner')
+    cy.get('#companyLink').type('www.google.com')
+    cy.get('.ms-auto').click()
+    cy.get('input[name=phone]').type('(518) 672-6861')
+    cy.get('.Header_input_section__1HbL7:nth-child(5) .Header_eye_icon__QRxrT').click()
 
+    /// get otp here START ///
+    cy.visit('https://oksms.org/receive/15186726861')
+    const userOtp = 1234
+
+    /// get otp here END ///
+
+    cy.xpath('//button[contains(.,\"Verify\")]').should('exist')
+    console.log('userOtp: ', userOtp)
+
+    cy.get('@userOtp').then((value) => {
+      const otpArray = value.split('')
+      const otpDigit1 = otpArray[0]
+      const otpDigit2 = otpArray[1]
+      const otpDigit3 = otpArray[2]
+      const otpDigit4 = otpArray[3]
+
+      cy.wrap(otpDigit1).as('otpDigit1')
+      cy.wrap(otpDigit2).as('otpDigit2')
+      cy.wrap(otpDigit3).as('otpDigit3')
+      cy.wrap(otpDigit4).as('otpDigit4')
+
+      console.log('otpDigits: ', otpDigit1, ' -- ', otpDigit2, ' -- ', otpDigit3, ' -- ', otpDigit4)
+
+      cy.get('.Header_otp_input_form__303Uy:nth-child(1) > input').type(otpDigit1)
+      cy.get('.Header_otp_input_form__303Uy:nth-child(2) > input').type(otpDigit2)
+      cy.get('.Header_otp_input_form__303Uy:nth-child(3) > input').type(otpDigit3)
+      cy.get('.Header_otp_input_form__303Uy:nth-child(4) > input').type(otpDigit4)
+    })
+
+    cy.get('.Header_login_btn__2tigy:nth-child(2)').click()
+
+    cy.xpath('//p[contains(.,\"Welcome\")]').should('contain.text', 'Welcome')
   })
 })

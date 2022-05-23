e8a0a39b1a3dd0a2d694860994f2805d08177198
ramondebruyn <78156608+ramondebruyn@users.noreply.github.com>
2022-05-23 13:55:01 -0400

    start implementing qa fixtures

--- cypress/integration/0findElements.spec.js
+++ cypress/integration/0findElements.spec.js
@@ -2,6 +2,34 @@
 /// <reference types="cypress-xpath" />
 /// <reference types="cypress-downloadfile"/>
 
-context('find elements on pages', () => {
+import qaElements from '../fixtures/qaElements.json'
 
+context('find elements on pages', () => {
+  it('check main page elements', () => {
+    cy.visit('https://staging.starlightmusic.com')
+    cy.get()
+  })
+
+  it('check our talent page', () => {
+    cy.visit('https://staging.starlightmusic.com/our-talent')
+    cy.get(qaElements.searchBlade.bookADemoButtonId).should('exist').click()
+    cy.get(qaElements.bookADemoModal1.fNameInputId).should('exist')
+    cy.get(qaElements.bookADemoModal1.lNameInputId).should('exist')
+    cy.get(qaElements.bookADemoModal1.phoneInputId).should('exist')
+    cy.get(qaElements.bookADemoModal1.emailInputId).should('exist')
+    cy.get(qaElements.bookADemoModal1.dateInputId).should('exist')
+    cy.get(qaElements.bookADemoModal1.venueInputId).should('exist')
+    cy.get(qaElements.bookADemoModal1.eventInputId).should('exist')
+    cy.get(qaElements.bookADemoModal1.bookADemoNextButtonId).should('exist')
+  })
+
+  it('check band page elements', () => {
+  })
+
+  it('checks admin portal elements', () => {
+    cy.visit('https://staging-starbridge.starlightmusic.com')
+  })
+
+  it('it checks client portal elements', () => {
+  })
 })
